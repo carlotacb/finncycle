261bdb15eabc33c8f8489cdae2816eacca228950
Eric Serrano <ersele46@gmail.com>
2022-11-04 22:09:20 +0000

    init spring

---
+++ server/wolt22/src/main/kotlin/com/junction/wolt22/Wolt22Application.kt
@@ -0,0 +1,11 @@
+package com.junction.wolt22
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Wolt22Application
+
+fun main(args: Array<String>) {
+    runApplication<Wolt22Application>(*args)
+}
---
+++ server/wolt22/src/test/kotlin/com/junction/wolt22/Wolt22ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junction.wolt22
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Wolt22ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
