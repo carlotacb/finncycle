952c9a6af50b44e87712dab9a892eff1a42e8164
EncryptEx <limusina10@gmail.com>
2022-11-05 04:56:32 +0200

    init post product

--- server/wolt22/src/main/kotlin/com/junction/wolt22/service/ProductServiceImpl.kt
+++ server/wolt22/src/main/kotlin/com/junction/wolt22/service/ProductServiceImpl.kt
@@ -1,11 +1,21 @@
 package com.junction.wolt22.service
 
 import com.junction.wolt22.beans.ProductDTO
+import com.junction.wolt22.domain.ProductEntity
+import com.junction.wolt22.repository.ProductRepository
 import org.springframework.stereotype.Service
 
 @Service
-class ProductServiceImpl : ProductService {
+class ProductServiceImpl(
+        private val productRepository: ProductRepository
+) : ProductService {
     override fun createProduct(productDTO: ProductDTO): Boolean {
-        TODO("Not yet implemented")
+        val productEntity = ProductEntity(
+                name = productDTO.name,
+                description = productDTO.description,
+                image = productDTO.image
+        )
+        val entity = productRepository.saveAndFlush(productEntity)
+        return entity.id > -1
     }
 }
