4d84cb1930ea5dc898020f546da4d7b8e5aee3ce
Carlota Catot Bragós <carkbra@gmail.com>
2022-11-05 21:18:05 +0100

    claim product and product page scafolding

--- client/finncycle/src/pages/available-products/index.tsx
+++ client/finncycle/src/pages/available-products/index.tsx
@@ -2,8 +2,11 @@ import styled from "styled-components";
 import Head from "next/head";
 import { faRecycle, faInfo } from "@fortawesome/free-solid-svg-icons";
 import ProductCard from "../../components/generic-components/ProductCard";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {H1} from "../../components/generic-components/general-styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {H1, StyledButton} from "../../components/generic-components/general-styled-components";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import {router} from "next/client";
 
 const Container = styled.div`
   display: flex;
@@ -12,6 +15,26 @@ const Container = styled.div`
   padding: 4%;
   width: 100%;
 `;
+
+const openModal = (id: string) => {
+  confirmAlert({
+    title: 'Are you sure?',
+    message: `Confirm that you want to claim the product with id: ${id}?`,
+    buttons: [
+      {
+        label: 'Yes',
+        onClick: () => claimProduct(id)
+      },
+      {
+        label: 'No'
+      }
+    ]
+  });
+};
+
+const claimProduct = (id: string) => {
+  console.log(`the product ${id} is claimed`)
+}
 
 export default function AvailableProducts() {
   return (
@@ -25,6 +48,31 @@ export default function AvailableProducts() {
           productName="Product Name"
           solidButton
           solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
+          solidButtonOnClick={() => openModal("test")}
+          solidButtonText="Claim"
+          outlineButton
+          outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
+          outlineButtonOnClick={() => {
+            router.push('/available-products/test');
+          }}
+        />
+        <ProductCard
+          productName="Product Name"
+          solidButton
+          solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
+          solidButtonOnClick={() => openModal("test")}
+          solidButtonText="Claim"
+          outlineButton
+          outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
+          outlineButtonOnClick={() => {
+            router.push('/available-products/test');
+          }}
+        />
+        <ProductCard
+          productName="Product Name"
+          solidButton
+          solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
+          solidButtonOnClick={() => openModal("test")}
           solidButtonText="Claim"
           outlineButton
           outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
@@ -33,22 +81,7 @@ export default function AvailableProducts() {
           productName="Product Name"
           solidButton
           solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
-          solidButtonText="Claim"
-          outlineButton
-          outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
-        />
-        <ProductCard
-          productName="Product Name"
-          solidButton
-          solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
-          solidButtonText="Claim"
-          outlineButton
-          outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
-        />
-        <ProductCard
-          productName="Product Name"
-          solidButton
-          solidButtonIcon={<FontAwesomeIcon icon={faRecycle}/>}
+          solidButtonOnClick={() => openModal("test")}
           solidButtonText="Claim"
           outlineButton
           outlineButtonIcon={<FontAwesomeIcon icon={faInfo}/>}
