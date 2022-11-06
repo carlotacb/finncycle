1b17b6bbb3a5b0ecf08137dfc2bb5d8f56d36d9a
jmiarons <joelmiarons@gmail.com>
2022-11-06 05:04:57 +0100

    CORS pls

--- server/wolt22/src/main/kotlin/com/junction/wolt22/config/WebConfigure.kt
+++ server/wolt22/src/main/kotlin/com/junction/wolt22/config/WebConfigure.kt
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfigure : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedMethods("*")
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*")
     }
 }
