99caccdb4147641f5eea008ffcd39fc06fc7c253
Carlota Catot Bragós <carkbra@gmail.com>
2022-11-06 03:07:39 +0100

    correct CD

--- client/finncycle/src/pages/index.tsx
+++ client/finncycle/src/pages/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import {GetServerSidePropsContext} from "next";
-import {getSession} from "../constants/utils";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  if (getSession() != null) {
-    return {
-      redirect: {
-        destination: `/available-products`,
-        permanent: false,
-      },
-    };
-  }
-
-  else {
-    return {
-      redirect: {
-        destination: `/login`,
-        permanent: false,
-      },
-    };
-  }
+  return {
+    redirect: {
+      destination: `/login`,
+      permanent: false,
+    },
+  };
 }
 
 export default function Home() {
