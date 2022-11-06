a25b7c7c2e1816d8cb35b96b325fee881b6b5475
Eric Serrano <ersele46@gmail.com>
2022-11-06 04:56:51 +0000

    intent fix cors 1

---
+++ server/wolt22/src/main/kotlin/com/junction/wolt22/config/WebConfigure.kt
@@ -0,0 +1,12 @@
+package com.junction.wolt22.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfigure : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
+}
